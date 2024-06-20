9591e6fab0b83d427e9c9b130881bf2dd7f35ce1
Diana Wiaduch <diana.wiaduch@onet.pl>
2024-06-20 20:42:56 +0200

    add third test

--- 03/app.js
+++ 03/app.js
@@ -3,7 +3,9 @@ export default function randomNumber(min, max) {
         throw new Error('Value is not a number');
     }
 
-
+    if ( min > max ) {
+        throw new Error('Min number is bigger than max number');
+    }
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
--- 03/app.test.js
+++ 03/app.test.js
@@ -14,3 +14,10 @@ test('check if arguments are type of number', () => {
 
     expect(() => randomNumber("S", 1)).toThrow('Value is not a number')
 })
+
+
+test('check if min number is bigger than max number', () => {
+
+
+    expect(() => randomNumber(2, 1)).toThrow('Min number is bigger than max number')
+})
