5a96cf33664ae0103818d7b54e3402a928c2d887
Diana Wiaduch <diana.wiaduch@onet.pl>
2024-06-22 13:26:04 +0200

    add task 05

--- 03/app.test.js
+++ 03/app.test.js
@@ -2,22 +2,15 @@ import randomNumber from './app';
 
 test('check if the result is 1', () => {
 
-
     expect(randomNumber(1, 1)).toBe(1)
-
-
-
 })
 
 test('check if arguments are type of number', () => {
 
-
     expect(() => randomNumber("S", 1)).toThrow('Value is not a number')
 })
 
-
 test('check if min number is bigger than max number', () => {
-
 
     expect(() => randomNumber(2, 1)).toThrow('Min number is bigger than max number')
 })
--- 05/DB.test.js
+++ 05/DB.test.js
@@ -1,1 +1,46 @@
 import DB from './DB';
+
+it('should throw exception when data is incorrect', () => {
+    function createWrongData() {
+        new DB({ a: z, b: 2 });
+    }
+
+    expect(createWrongData).toThrow();
+});
+
+it("should throw exception when ID is duplicated", async () => {
+
+    try {
+        const example = new DB({ a: 3, b: 5 })
+        await example.insert({ a: 3 });
+    }
+    catch (err) {
+
+        expect(err).toBe("'ID can\'t be duplicated!'");
+    }
+
+});
+
+it("should throw exception when item doesn't exist", async () => {
+    try {
+        const example = new DB({ a: 3, b: 5 })
+        await example.remove({ a: 6 });
+    }
+    catch (err) {
+
+        expect(err).toBe("Item not exist!");
+    }
+});
+it("should throw exception when ID is not set", async () => {
+    try {
+        const example = new DB({ a: 3, b: 5 })
+        await example.update({ a: 3, b: 5 });
+    }
+    catch (err) {
+
+        expect(err).toBe("ID have to be set!");
+    }
+
+});
+
+
